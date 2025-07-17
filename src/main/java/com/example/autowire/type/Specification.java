8339c10d9d6aa30d8cef414ffb6e451ced6ff0d1
anhquanle <75654249+anhquanle@users.noreply.github.com>
2025-07-17 15:44:28 +0700

    Autowiring

---
+++ src/main/java/com/example/autowire/type/Specification.java
@@ -0,0 +1,22 @@
+package com.example.autowire.type;
+
+public class Specification {
+    private String make;
+    private  String model;
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
