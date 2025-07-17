91118149710ba3144c8eb4bf3238b94b57af233a
anhquanle <75654249+anhquanle@users.noreply.github.com>
2025-07-17 16:46:29 +0700

    Spring end

---
+++ src/main/java/com/example/autowire/annotation/Manager.java
@@ -0,0 +1,29 @@
+package com.example.autowire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("manager")
+public class Manager {
+    @Autowired
+    @Qualifier("employee")
+    private Employee employee;
+
+//    @Autowired
+//    public Manager(Employee employee) {
+//        this.employee = employee;
+//    }
+
+//    @Autowired
+//    public void setEmployee(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/conponentscan_annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.conponentscan_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.conponentscan_annotation")
+public class AppConfig {
+}
